d7621560b3246045373e81383ad0205b3e4ca03e
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-05-03 19:27:40 -0300

    adiciona a resolução do exercício 3

---
+++ 01_fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios/script3.js
@@ -0,0 +1,17 @@
+let num1 = 15;
+let num2 = 20;
+let num3 = 19;
+
+let maior = 0;
+
+if(num1 > num2) {
+    maior = num1;
+} else {
+    maior = num2;
+} 
+
+if (maior > num3) {
+    console.log(maior);
+} else {
+    console.log(num3);
+}
