504713b073f9e2a67dcc8738c4f4ef981c47fffc
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-05-03 20:23:52 -0300

    altera a resolução do exercício 8

--- 01_fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios/script8.js
+++ 01_fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios/script8.js
@@ -1,11 +1,12 @@
 /* 8 - Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, o código deve retornar false. */
 
-let num1 = 6;
+let num1 = 5;
 let num2 = 3;
 let num3 = 7;
+let result = false;
 
 if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
-    console.log(true);
-} else {
-    console.log(false);
+    result = true;
 }
+
+console.log(result);
