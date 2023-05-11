4c2a27130550cf9220f9b77d7530d71168e6152d
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-05-11 19:24:34 -0300

    adiciona a resolução do exercício de fixar da página qual seletor usar?

---
+++ 01_fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/qual-seletor-usar/exercicio01/script.js
@@ -0,0 +1,14 @@
+const header = document.getElementById('header-container');
+header.style.backgroundColor = 'rgb(0,176,104)';
+
+const sections = document.getElementsByTagName('section');
+sections[0].style.backgroundColor = 'rgb(255,160,132)';
+sections[1].style.backgroundColor = 'rgb(249,220,93)';
+
+const h3Tags = document.getElementsByTagName('h3');
+console.log(h3Tags);
+h3Tags[0].style.background = 'rgb(165,0,244)';
+h3Tags[1].style.background = 'rgb(165,0,244)';
+h3Tags[2].style.background = 'rgb(35,37,36)';
+h3Tags[3].style.background = 'rgb(35,37,36)';
+
